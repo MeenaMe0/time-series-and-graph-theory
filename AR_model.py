1d16f2017442f81e382ad84c4e97dbf06511228e
_meena.me <111679567+MeenaMe0@users.noreply.github.com>
2023-01-18 08:34:41 +0700

    Update AR_model.py

--- AR_model.py
+++ AR_model.py
@@ -9,11 +9,11 @@ white_noise = np.random.randn(num_steps)      #normal random numstep value
 # Generate the ma model at thie T ,by adding value from time T-1 and T-2
 ar_model = [0,0]
 for i in range (2,num_steps) :
-    # Add the random value to list of random variable : binary_p 
+    # Add the random value to list of random variable : ar_model
     # -0.5 * X_t-2 + 0.75 * X_t-1 + W_t
    ar_model.append(-0.5*ar_model[i-2]+0.75*ar_model[i-1]+white_noise[i])
 
-# Plot the Binary process
+# Plot the ar_model
 plt.plot(ar_model)
 
 # Draw the mean or expected value line : y = 0
