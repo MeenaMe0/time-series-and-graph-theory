386a8a36bec90df229863492036e8778707e99ce
_meena.me <111679567+MeenaMe0@users.noreply.github.com>
2023-01-18 08:13:19 +0700

    Update MA_model.py

--- MA_model.py
+++ MA_model.py
@@ -9,7 +9,7 @@ white_noise = np.random.randn(num_steps+2)      #normal random numstep+2 value
 # Generate the ma model at thie T ,by adding white_noise from time T ,T-1 and T-2
 ma_model = [0,0]
 for i in range (2,num_steps) :
-    # Add the random value to list of random variable : binary_p 
+    # Add the random value to list of random variable : ma_model
    ma_model.append(white_noise[i-2]+white_noise[i-1]+white_noise[i])
 
 # Plot the Binary process
